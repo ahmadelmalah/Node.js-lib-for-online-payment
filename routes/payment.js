c611b15f6788d7b96e2a3b2020f539f37070154f
ahmadelmalah <ahmedelmalah@hotmail.com>
2019-06-13 00:28:07 +0200

    Handling real  credit card payment

--- routes/payment.js
+++ routes/payment.js
@@ -13,8 +13,47 @@ router.post('/', function(req, res, next) {
   });
 
   gateway.transaction.sale({
-    amount: '5.00',
+    amount: req.body.amount,
     paymentMethodNonce: 'nonce-from-the-client',
+    customer: {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      company: req.body.company,
+      phone: req.body.phone,
+      fax: req.body.fax,
+      email: req.body.email,
+      website: req.body.website
+    },
+    creditCard: {
+      cardholderName: req.body.cardholderName,
+      number: req.body.cardNumber,
+      cvv: req.body.cardCvv,
+      expirationMonth: req.body.cardExpirationMonth,
+      expirationYear: req.body.cardExpirationYear
+    },
+    //Blilling and shipping data are dummmy, just for protopying and testing.
+    billing: {
+      firstName: "Paul",
+      lastName: "Smith",
+      company: "Braintree",
+      streetAddress: "1 E Main St",
+      extendedAddress: "Suite 403",
+      locality: "Chicago",
+      region: "IL",
+      postalCode: "60622",
+      countryCodeAlpha2: "US"
+    },
+    shipping: {
+      firstName: "Jen",
+      lastName: "Smith",
+      company: "Braintree",
+      streetAddress: "1 E 1st St",
+      extendedAddress: "5th Floor",
+      locality: "Bartlett",
+      region: "IL",
+      postalCode: "60103",
+      countryCodeAlpha2: "US"
+    },
     options: {
       submitForSettlement: true
     }
@@ -28,7 +67,7 @@ router.post('/', function(req, res, next) {
       res.send('Transaction ID: ' + result.transaction.id);
     } else {
       console.error(result.message);
-      res.send('Payment is not done!');
+      res.send('Payment Faild!');
     }
   });
 
