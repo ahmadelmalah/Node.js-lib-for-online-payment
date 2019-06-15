84f47be3fa4f84037b8b087e0ddde7d3bc323887
ahmadelmalah <ahmedelmalah@hotmail.com>
2019-06-15 04:15:58 +0200

    Refactoring

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bosta Payment Platform' });
+  res.render('index', {
+    title: 'Bosta Payment Platform',
+    amount: process.env.AMOUNT,
+    paypal_client_id: process.env.PAYPAL_CLIENT_ID
+  });
 });
 
 module.exports = router;
